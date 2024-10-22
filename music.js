5895bc4ab3a8357deaebed90d250c1f9372ab65e
Gintarasxd <36343955+Gintarasxd@users.noreply.github.com>
2024-10-22 19:33:50 +0300

    Update music.js

--- music.js
+++ music.js
@@ -6,3 +6,17 @@ document.addEventListener("DOMContentLoaded", function() {
         navMenu.classList.toggle('active');  // Įjungiamas/išjungiamas meniu
     });
 });
+
+// Pasirinkti visus <li> elementus iš meniu
+const menuItems = document.querySelectorAll('nav ul li a');
+
+// Pridėti įvykį kiekvienam elementui
+menuItems.forEach(item => {
+    item.addEventListener('click', function() {
+        // Panaikinti 'active' klasę iš visų elementų
+        menuItems.forEach(link => link.parentElement.classList.remove('active'));
+
+        // Pridėti 'active' klasei šiuo metu spustelėtam elementui
+        this.parentElement.classList.add('active');
+    });
+});
